962b7519366e7d6e036cb45ed2777953b608de7c
Jim Barritt <jimb@thoughtworks.com>
2019-08-18 10:19:54 +0100

    Moving log init

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use log::{info};
 use simple_logger;
 
 fn main() {
+    simple_logger::init().unwrap();
     say_hello("foo");
 }
 
 fn say_hello(name: &str) {
-    simple_logger::init().unwrap();
     info!("Hello, {name}!", name = name);
 }

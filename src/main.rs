0b49c2d46868f82dfa00e42da14ea5eaee6176ef
Jim Barritt <jimb@thoughtworks.com>
2019-08-18 16:54:59 +0100

    Adding command line args processing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use log::{info};
-use simple_logger;
+extern crate log;
+extern crate simple_logger;
+extern crate clap;
+
+use log::{info, debug};
+use clap::{Arg, App, ArgMatches};
+use std::env;
+
 
 fn main() {
     simple_logger::init().unwrap();
+
+    let args = parse_cmd_line_args();
+
+    info!("Verbose: {:?}", args.is_present("v"));
+
     say_hello("foo");
 }
 
 fn say_hello(name: &str) {
     info!("Hello, {name}!", name = name);
+}
+
+// see https://github.com/clap-rs/clap#quick-example
+fn parse_cmd_line_args<'a>() -> ArgMatches<'a> {
+    debug!("{:?}", env::args_os());
+    let matches = App::new("Rustlang Demo")
+        .version("1.0")
+        .author("Jim Barritt <jimb@thoughtworks.com")
+        .about("Shows some basic application tasks in rust, including a little api server")
+        .arg(Arg::with_name("v")
+            .short("v")
+            .long("verbose")
+            .help("Sets the level of verbosity"))
+        .get_matches();
+
+    return matches;
+
 }
